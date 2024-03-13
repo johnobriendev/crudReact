fb92c2077dcaf9f57a4a414e3f3f883dcca07af9
John O'Brien <johnobrien.dev@gmail.com>
2024-03-13 14:31:38 -0400

    create addTodo function in app component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import todoInput from './Components/Input';
 function App() {
   const [todos, setTodos] = useState([]); // Array to store todos
   
+  const addTodo = (text) => {
+    setTodos([
+      ...todos,
+      { id: Math.random(), text: text },
+    ]);
+  };
+  
+
   return(
     <div className='app'>
       <todoInput addTodo={addTodo}/>
