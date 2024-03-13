53d27dbf0bbc2eddc914ed4206ce7163ca16056a
John O'Brien <johnobrien.dev@gmail.com>
2024-03-13 13:49:28 -0400

    create app component and useState for todos

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+import React, { useState } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState([]); // Array to store todos
+  
+  return(
+    <div className='app'>
+
+    </div>
+  );
+}
+
+export default App;
