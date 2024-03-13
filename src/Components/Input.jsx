38185e82822ceb0337b431e9d76d6dbcb02b9816
John O'Brien <johnobrien.dev@gmail.com>
2024-03-13 14:08:34 -0400

    add form to input component

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -6,6 +6,15 @@ function todoInput({addTodo}){
     const [inputText, setInputText] = useState('');
     
     return(
+        <form>
+            <input 
+            type="text"
+            value = {inputText}
+            onChange={(e) => setText(e.target.value)}
+            placeholder='Write something...'
 
+            />
+            <button type='submit'>Add</button>
+        </form>
     );
 }
