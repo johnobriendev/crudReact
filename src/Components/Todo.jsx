f3f8b114e30610df7a021e4616e4f33740cf0196
John O'Brien <johnobrien.dev@gmail.com>
2024-03-13 18:55:23 -0400

    add edit function to todoitem

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 function TodoItem ({todo , removeTodo}){
+    const [isEditing, setIsEditing] = useState(false);
+    const [editText, setEditText] = useState(todo.text);
+
+    const handleEdit = () => {
+        setIsEditing(!isEditing);
+        setEditText(todo.text);
+      };
+
     return (
         <li key={todo.id}> 
-        {todo.text}
-        <button onClick={() => removeTodo(todo.id)}>Delete</button>
-         </li>
+        
+            {isEditing ? (
+                <input
+                    type="text"
+                    value={editText}
+                    onChange={(e) => setEditText(e.target.value)}
+                />
+                ) : (
+                {todo.text}
+                )}
+            
+            <button onClick={handleEdit}> {isEditing ? 'Save' : 'Edit'} </button>
+            <button onClick={() => removeTodo(todo.id)}>Delete</button>
+        </li>
       );
 }
 
