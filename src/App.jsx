671ecf93018cb714d9361d06f68a45f9fbca6869
John O'Brien <johnobrien.dev@gmail.com>
2024-03-13 14:25:13 -0400

    add input to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-
-import './App.css'
-
 import React, { useState } from 'react';
+import './App.css'
+import todoInput from './Components/Input';
 
 function App() {
   const [todos, setTodos] = useState([]); // Array to store todos
   
   return(
     <div className='app'>
-
+      <todoInput addTodo={addTodo}/>
     </div>
   );
 }
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -5,16 +5,27 @@ function todoInput({addTodo}){
     
     const [inputText, setInputText] = useState('');
     
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(inputText){
+            addTodo(inputText);
+            setInputText ('');
+        }
+    }
+
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <input 
             type="text"
             value = {inputText}
             onChange={(e) => setText(e.target.value)}
             placeholder='Write something...'
-
             />
             <button type='submit'>Add</button>
         </form>
     );
 }
+
+export default todoInput;
